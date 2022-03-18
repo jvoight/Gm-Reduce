d25062947fab631b7796b493a4161cd2c22c30ab
Sam Schiavone <sam.schiavone@gmail.com>
2022-03-18 16:22:32 -0400

    more script

--- script.m
+++ script.m
@@ -1,6 +1,6 @@
 load "config.m";
 AttachSpec("../Gm-Reduce/spec");
-AttachSpec("../Belyi/spec");
+//AttachSpec("../Belyi/Code/spec");
 s := BelyiDBRead(filename);
 if BelyiMapIsComputed(s) then
   print BelyiDBToRows(s);
