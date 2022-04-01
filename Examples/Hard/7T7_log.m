2198b86bd586d3ea5db72bf91fd1720f036dea7c
Ciaran Schembri <ciaran.schembri@dartmouth.edu>
2022-04-01 17:21:53 -0400

    Trial reduction for genus 1

---
+++ Examples/Hard/7T7_log.m
@@ -0,0 +1,13 @@
+Magma V2.26-10    Fri Apr  1 2022 17:19:05 on LEGENDRE [Seed = 3164192095]
+
++-------------------------------------------------------------------+
+|       This copy of Magma has been made available through a        |
+|                   generous initiative of the                      |
+|                                                                   |
+|                         Simons Foundation                         |
+|                                                                   |
+| covering U.S. Colleges, Universities, Nonprofit Research entities,|
+|               and their students, faculty, and staff              |
++-------------------------------------------------------------------+
+
+Type ? for help.  Type <Ctrl>-D to quit.
