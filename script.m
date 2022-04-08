6c0df389eb296749c3bd78e3476dddad31cc26b9
SamSchiavone <sam.schiavone@gmail.com>
2022-04-08 01:14:47 -0400

    update script

--- script.m
+++ script.m
@@ -3,7 +3,16 @@ AttachSpec("code/spec_triangle");
 AttachSpec("../Gm-Reduce/spec");
 //AttachSpec("../Belyi/Code/spec");
 s := BelyiDBRead(filename);
-if BelyiMapIsComputed(s) then
-  print BelyiDBToRows(s);
+Ks := [el[1] : el in s`BelyiDBBaseFieldData];
+d := Max([Degree(el) : el in Ks]);
+if BelyiMapIsComputed(s) and (d le 20) then
+  try
+    print BelyiDBToRows(s);
+  catch e
+    print "================================";
+    printf "%o failed\n", s`BelyiDBFilename;
+    print e`Object;
+    print "================================";
+  end try;
 end if;
 quit;
